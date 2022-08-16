6032085f824a002d4f3b4a68b5ee077a2e57a40b
eliot-akira <me@eliotakira.com>
2022-08-16 15:10:15 +0200

    Add BetterDash and BetterLifter as Composer dependencies

--- tangible-loops-and-logic-pro.php
+++ tangible-loops-and-logic-pro.php
@@ -9,7 +9,7 @@
  * License: GPLv2 or later
  */
 
-define( 'TANGIBLE_LOOPS_AND_LOGIC_PRO', '3.0.0' );
+define( 'TANGIBLE_LOOPS_AND_LOGIC_PRO_VERSION', '3.0.0' );
 
 require_once __DIR__ . '/vendor/tangible/plugin-framework/index.php';
 require_once __DIR__ . '/vendor/tangible/plugin-updater/index.php';
@@ -31,7 +31,7 @@ add_action('plugins_loaded', function() {
     'title'          => 'Loops & Logic Pro',
     'setting_prefix' => 'tloopslogicpro',
 
-    'version'        => TANGIBLE_LOOPS_AND_LOGIC_PRO,
+    'version'        => TANGIBLE_LOOPS_AND_LOGIC_PRO_VERSION,
     'file_path'      => __FILE__,
     'base_path'      => plugin_basename( __FILE__ ),
     'dir_path'       => plugin_dir_path( __FILE__ ),
